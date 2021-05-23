144b42e0756950308931161a810590eafbc47c2d
ArtIrina <80789112+ArtIrina@users.noreply.github.com>
2021-05-23 11:52:03 +0300

    Update ODBC_2.py

--- ODBC_2.py
+++ ODBC_2.py
@@ -8,28 +8,28 @@ connection_string = "Driver={ODBC Driver 17 for SQL Server};"\
 "Database=Cinema;"\
 "uid=ucinema;pwd=P@sswordCinema;"
 
-# print(connection_string)
-
 conn = pyodbc.connect(connection_string)
 # Подключились к SQL серверу
 
 # Нужно создать таблицу  https://proglib.io/p/kak-podruzhit-python-i-bazy-dannyh-sql-podrobnoe-rukovodstvo-2020-02-27
+# https://blog.finxter.com/creating-a-database-table-using-the-package-pyodbc-in-python/
 # table
 cursor = conn.cursor()
 
 cursor.execute('''
-CREATE TABLE Kinoagent (
+CREATE TABLE Kinoagentstvo (
 Link TEXT,
 Description TEXT,
 Photo TEXT,
 Address TEXT,
 Price integer,
 );
-''') # Чтобы таблица появилась
+''')
 conn.commit()
 
 with open("data_file.json", "r") as write_file:
     info = json.load(write_file)
+
 # Теперь внесем все наши данные в таблицу
 for obj in range(len(info)):
     link = info[obj]['link']
@@ -40,7 +40,9 @@ for obj in range(len(info)):
     address = info[obj]['header']
     price = info[obj]['price']
 
-    sql = "INSERT INTO Kinoagent (Link, Description, Photo, Address, Price) VALUES  (?, ?, ?, ?, ?)"
+    sql = "INSERT INTO Kinoagentstvo (Link, Description, Photo, Address, Price) VALUES  (?, ?, ?, ?, ?)"
     val = [(link, description, photo, address, price)]
     cursor.executemany(sql, val)
     conn.commit()
+
+conn.close()
